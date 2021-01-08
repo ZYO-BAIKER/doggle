7a1883495abd4093c98dbb36a8b45aa23ba18564
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-01-08 09:15:16 +0900

    Userモデルにバリデーション設定
    nickname（10文字以内）、prefecture_id、age_id、sex_id、city、が入力必須
    passwordが半角英数字混合である

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,15 @@ class User < ApplicationRecord
   belongs_to_active_hash :sex
   belongs_to_active_hash :age
   belongs_to_active_hash :prefecture
+  
+  validates :nickname, presence: true, length: { maximum: 10 }
+  validates :city,  presence: true
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  with_options numericality: { other_than: 1 } do
+    validates :sex_id, :age_id, :prefecture_id
+  end
+  
 end
