f865721b73e9acd2d78ed4b6a09a9dea48435beb
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-01-09 13:58:42 +0900

    dogsテーブルのマイグレーション
    カラム設定

---
+++ db/migrate/20210108120232_create_dogs.rb
@@ -0,0 +1,12 @@
+class CreateDogs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :dogs do |t|
+      t.string     :name,           null: false, default: ""
+      t.integer    :type_id,        null: false
+      t.integer    :sex_id,         null: false
+      t.text       :description,    null: false
+      t.references :user,           null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
