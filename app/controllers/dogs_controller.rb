94f5fad29c8a20a669dd77f886c2828a706e5923
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-01-13 20:40:04 +0900

    dogコントローラー
    edit,update,destoryアクションを新たに定義

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,5 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:edit, :update, :destroy]
 
   def index
   end
@@ -17,10 +18,34 @@ class DogsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @dog.update(dog_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if user_signed_in? && current_user.id == @dog.user_id
+      @dog.destroy
+      redirect_to root_path
+    else
+      redirect_to dog_path
+    end
+  end
+
   private
 
   def dog_params
     params.require(:dog).permit(:image, :name, :description, :type_id, :dog_sex_id).merge(user_id: current_user.id)
+  end
+
+  def set_dog
+    @dog = Dog.find(params[:id])
   end
 
 end
