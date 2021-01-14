8ca13218bf0bf22048e922ced65a1622e1df0d83
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-01-14 18:34:44 +0900

    comments ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
   devise_for :users
-  get 'dogs/index'
   root to: "dogs#index"
   
   resources :dogs
+
   resources :users, only: [:show, :update, :edit]
+
+  resources :comments, only:[:create,:update,:destroy]
+
 end
