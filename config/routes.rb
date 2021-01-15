b8accaca96a6eac1c73a66b966e35630e35f3a44
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-01-15 13:51:05 +0900

    ルーティング
    commentsをdogsのネストに設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "dogs#index"
   
-  resources :dogs
-
   resources :users, only: [:show, :update, :edit]
 
-  resources :comments, only:[:create,:update,:destroy]
+  resources :dogs do
+    resources :comments, only:[:create,:update,:destroy]
+    end
+  end
 
 end
