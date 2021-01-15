a28438a258599809ba758d658e9628abe625dd11
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-01-15 14:31:10 +0900

    コントローラーの定義
    CommentsコントローラーにActionCableの設定
    dogsコントローラーのshowアクションに@commentsと@commentを定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,12 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.create(comment_params)
+    ActionCable.server.broadcast 'comment_channel', content: @comment, user: @comment.user if @comment.save
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, dog_id: params[:dog_id])
   end
 end
