5aea494f5d2e91167cc1b91e118889e3dc241a30
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-01-17 01:20:12 +0900

    FactoryBotに追加事項
    userにpasseordを手入力
    dogにimageを追加

--- spec/factories/dogs.rb
+++ spec/factories/dogs.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :dog do
     name { Faker::Lorem.sentence }
     description { Faker::Lorem.sentence }
+    image {Faker::Lorem.sentence}
     type_id { 2 }
     dog_sex_id  { 3 }
     association :user
