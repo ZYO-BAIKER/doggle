e76e7b742c607737917e6c53d3b1762813af9ef9
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-03-05 18:27:42 +0900

    ストロングパラメーターに、imageを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :prefecture_id, :city, :sex_id, :age_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :prefecture_id, :city, :sex_id, :age_id, :image])
   end
 
 end
