093c276c8c8aeabbd321a5b1dd364200b0bd79fc
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-03-13 21:06:13 +0900

    コントローラーに定義追加
    searchアクション
    ransackで「犬種」「犬性別」「ユーザー性別」「ユーザー年齢」「都道府県」の検索条件を追加
    いずれもプルダウンメニューでの選択式

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,8 +2,11 @@ class DogsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   before_action :set_dog, only: [:show, :edit, :update, :destroy]
   before_action :return, only: [:edit]
+  before_action :search_dog, only: [:index, :search]
 
   def index
+    set_dog_column
+    set_user_column
   end
 
   def new
@@ -46,6 +49,10 @@ class DogsController < ApplicationController
     end
   end
 
+  def search
+    @results = @p.result.includes(:user)  # 検索条件にマッチした商品の情報を取得
+  end
+
   private
 
   def dog_params
@@ -60,4 +67,18 @@ class DogsController < ApplicationController
     redirect_to root_path unless user_signed_in? && current_user.id == @dog.user_id
   end
 
+  def search_dog
+    @p = Dog.ransack(params[:q])  # 検索オブジェクトを生成
+  end
+
+  def set_dog_column
+    @dog_type_id = Type.where.not(id: 1)
+    @dog_sex_id = DogSex.where.not(id: 1) 
+  end
+
+  def set_user_column
+    @user_sex_id = Sex.where.not(id: 1) 
+    @user_age_id = Age.where.not(id: 1)
+    @user_prefecture_id = Prefecture.where.not(id: 1)
+  end
 end
