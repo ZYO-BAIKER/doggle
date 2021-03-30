d8b8623519c96613acda7bee5c4fd9357bc0164d
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-03-30 19:53:31 +0900

    プレビュー機能の修正
    user登録でも機能するように改善

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,4 +1,4 @@
-if (document.URL.match( /new/ ) ) {
+if ( document.URL.match( /new/ ) || document.URL.match( /sign_up/ ) )  {
 
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
