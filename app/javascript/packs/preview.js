80eae8f05b4a9f520410db45d825271da3f9f6bf
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-04-01 01:06:40 +0900

    プレビュー 機能の場合分け
    userのavatarは一枚のみなので、今まで通りのプレビュー 機能
    dogは複数枚投稿に併せて、選択ボタンが追加されるように改善

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,4 +1,53 @@
-if ( document.URL.match( /new/ ) || document.URL.match( /sign_up/ ) || document.URL.match( /edit/ ) )  {
+if ( document.URL.match( /dogs/ )  )  {
+
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+
+    // 選択した画像を表示する関数
+    const createImageHTML = (blob) => {
+      // 画像を表示するためのdiv要素を生成
+     const imageElement = document.createElement('div');
+     imageElement.setAttribute('class', "image-element")
+     let imageElementNum = document.querySelectorAll('.image-element').length
+
+     // 表示する画像を生成
+     const blobImage = document.createElement('img');
+     blobImage.setAttribute('src', blob);
+     blobImage.setAttribute('class', 'image-size');
+     blobImage.setAttribute('height', '150');
+
+      // ファイル選択ボタンを生成
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `show_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'dog[images][]')
+      inputHTML.setAttribute('type', 'file')
+
+     // 生成したHTMLの要素をブラウザに表示させる
+     imageElement.appendChild(blobImage);
+     imageElement.appendChild(inputHTML);
+     ImageList.appendChild(imageElement);
+
+     inputHTML.addEventListener('change', (e) => {
+      file = e.target.files[0];
+      blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob)
+    })
+   };
+
+    document.getElementById("show-image").addEventListener('change', function(e){
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      
+      createImageHTML(blob);
+    });
+    
+  });
+ 
+}
+
+if (  document.URL.match( /users/ )  )  {
 
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
@@ -14,6 +63,7 @@ if ( document.URL.match( /new/ ) || document.URL.match( /sign_up/ ) || document.
      blobImage.setAttribute('class', 'image-size');
      blobImage.setAttribute('height', '150');
 
+
      // 生成したHTMLの要素をブラウザに表示させる
      imageElement.appendChild(blobImage);
      ImageList.appendChild(imageElement);
@@ -26,13 +76,10 @@ if ( document.URL.match( /new/ ) || document.URL.match( /sign_up/ ) || document.
       if (imageContent){
         imageContent.remove();
       }
-
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
       
       createImageHTML(blob);
     });
-    
   });
- 
 }
