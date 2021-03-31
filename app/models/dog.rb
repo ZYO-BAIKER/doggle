5d08637c61ed6b716707ca8f2844c8965892e64c
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-04-01 01:05:11 +0900

    各ファイルをimagesへ変更
    dogモデル、コントローラー、ビュー、エラーメッセージをimageからimagesへ変更

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,5 @@
 class Dog < ApplicationRecord
-  has_one_attached :image
+  has_many_attached :images
   belongs_to :user
   has_many :comments, dependent: :destroy
 
@@ -8,7 +8,7 @@ class Dog < ApplicationRecord
   belongs_to_active_hash :type
 
   with_options presence: true do
-    validates :image, :name, :description
+    validates :images, :name, :description
   end
 
   with_options numericality: { other_than: 1, message: 'を選んで下さい' } do
